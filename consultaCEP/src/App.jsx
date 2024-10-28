be9367007c0d358abcf0eaa47e6dc29b2c9b4501
Bruno Vitor Pires <bvpn20@gmail.com>
2024-10-28 19:59:42 -0300

    criando historico :)

--- consultaCEP/src/App.jsx
+++ consultaCEP/src/App.jsx
@@ -1,62 +1,101 @@
-import { useState } from "react";
-import "./App.css";
-import axios from "axios";
+  import { useEffect, useState } from "react";
+  import "./App.css";
+  import axios from "axios";
 
-function App() {
-  const endPoint = `https://viacep.com.br/ws/`;
-  const [cepData, setCepData] = useState({});
-  const [inputCep, setInputCep] = useState(""); // variável para capturar o valor do input
+  function App() {
+    const endPoint = `https://viacep.com.br/ws/`;
+    const endCEP = `http://localhost:3000/cep`
+    const [cepData, setCepData] = useState({});
+    const [inputCep, setInputCep] = useState(""); // variável para capturar o valor do input
+    const [cep, setCEP] = useState([])
 
-  const buscaCEP = async () => {
-    try {
-      const response = await axios.get(`${endPoint}${inputCep}/json`); // Usando o valor do inputCep
-      setCepData(response.data);
-    } catch (error) {
-      console.log("Erro ao consultar cep", error);
+    const buscaCEP = async () => {
+      try {
+        const response = await axios.get(`${endPoint}${inputCep}/json`); // Usando o valor do inputCep
+        setCepData(response.data);
+      } catch (error) {
+        console.log("Erro ao consultar cep", error);
+      }
+    };
+
+    const salvarCEP = async() =>{
+
+      try{
+
+        const response = await axios.post(`${endCEP}`,cepData)
+        console.log('CEP inserido',response.data)
+
+      }catch(error){
+        console.log('Erro ao salvar', error)
+      }
+
     }
-  };
 
-  return (
-    <>
-      <label htmlFor="buscaCEP">Digite seu CEP </label>
-      <input
-        type="text"
-        name="buscaCEP"
-        id="buscaCEP"
-        value={inputCep} // Valor do input controlado pela nova variável de estado
-        onChange={(e) => setInputCep(e.target.value)} // Atualizando o valor digitado
-      />
-      {inputCep}
-      <button type="button" onClick={buscaCEP}>
-        Pesquisar
-      </button>
+    const carregarCEP = async()=>{
+      try{
 
-      {/* Exibindo os dados do CEP, se disponíveis */}
-      {cepData.cep ? (
-        // Se o CEP for encontrado, exibe os dados
-        <div>
-          <p>
-            <strong>CEP:</strong> {cepData.cep}
-          </p>
-          <p>
-            <strong>Logradouro:</strong> {cepData.logradouro}
-          </p>
-          <p>
-            <strong>Bairro:</strong> {cepData.bairro}
-          </p>
-          <p>
-            <strong>Cidade:</strong> {cepData.localidade}
-          </p>
-          <p>
-            <strong>UF:</strong> {cepData.uf}
-          </p>
-        </div>
-      ) : (
-        // Senão, exibe uma mensagem alternativa
-        <p>CEP não encontrado ou ainda não pesquisado.</p>
-      )}
-    </>
-  );
-}
+        const response = await axios.get(`${endCEP}`);
+        setCEP(response.data);
+      }catch(error){
+        console.log('Erro ao carregar',error)
+      }
+    }
 
-export default App;
+    useEffect(()=>{
+      carregarCEP();
+    })
+
+    return (
+      <>
+        <label htmlFor="buscaCEP">Digite seu CEP </label>
+        <input
+          type="text"
+          name="buscaCEP"
+          id="buscaCEP"
+          value={inputCep} // Valor do input controlado pela nova variável de estado
+          onChange={(e) => setInputCep(e.target.value)} // Atualizando o valor digitado
+        />
+        {/* {inputCep} */}
+        <button type="button" onClick={buscaCEP}>
+          Pesquisar
+        </button>
+        <button type="button" onClick={salvarCEP}>
+          Salvar
+        </button>
+
+        {/* Exibindo os dados do CEP, se disponíveis */}
+        {cepData.cep ? (
+          // Se o CEP for encontrado, exibe os dados
+          <div>
+            <p>
+              <strong>CEP:</strong> {cepData.cep}
+            </p>
+            <p>
+              <strong>Logradouro:</strong> {cepData.logradouro}
+            </p>
+            <p>
+              <strong>Bairro:</strong> {cepData.bairro}
+            </p>
+            <p>
+              <strong>Cidade:</strong> {cepData.localidade}
+            </p>
+            <p>
+              <strong>UF:</strong> {cepData.uf}
+            </p>
+          </div>
+        ) : (
+          // Senão, exibe uma mensagem alternativa
+          <p>CEP não encontrado ou ainda não pesquisado.</p>
+        )}
+        <h1>Histórico</h1>
+        {cep.map((cep) => (
+          <div className="cep" key={cep.id}>
+          <h3>{cep.cep}</h3>
+          </div>
+        ))}
+        
+      </>
+    );
+  }
+
+  export default App;
